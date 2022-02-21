312f442c96af88fdf87d10be8d0905d10d2c10b5
jguibrandao <jguibrandao@gmail.com>
2022-02-21 06:54:36 -0300

    Creating UserSchema and using a pre save function that encrypts the password

---
+++ src/users/schemas/user.schema.ts
@@ -0,0 +1,29 @@
+import * as mongoose from "mongoose";
+import * as bcrypt from "bcrypt";
+
+export const UsersSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+UsersSchema.pre('save', async function(next) {
+    try {
+        if(!this.isModified('password')) {
+            return next()
+        }
+
+        this['password'] = await bcrypt.hash(this['password'], 10)
+    } catch (err) {
+        return next(err)
+    }
+})
