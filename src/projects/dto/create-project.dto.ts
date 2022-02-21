3a48ae4c645277e5c5277604410d25c37c8c1048
jguibrandao <jguibrandao@gmail.com>
2022-02-21 18:24:31 -0300

    setting create project dto

--- src/projects/dto/create-project.dto.ts
+++ src/projects/dto/create-project.dto.ts
@@ -1,1 +1,46 @@
-export class CreateProjectDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { IsBoolean, IsISO8601, IsNotEmpty, IsString } from "class-validator";
+
+export class CreateProjectDto {
+    @ApiProperty({
+        description: "A string type name with 1 or more letters",
+        example: "John"
+    })
+    @IsNotEmpty()
+    @IsString()
+    name: string;
+
+    @ApiProperty({
+        description: "A string type description for your project",
+        example: "This project uses NestJS and mongodb"
+    })
+    @IsNotEmpty()
+    @IsString()
+    description?: string;
+
+    @ApiProperty({
+        description: "A date type that tells when you started your project (use - not /)",
+        example: "2004-06-23"
+    })
+    @IsNotEmpty()
+    @IsString()
+    @IsISO8601()
+    initDate: string;
+
+    @ApiProperty({
+        description: "A date type that tells when you finished your project (use - not /)",
+        example: "2004-06-23"
+    })
+    @IsNotEmpty()
+    @IsString()
+    @IsISO8601()
+    end?: string;
+
+    @ApiProperty({
+        description: "A boolean that tells if your project is active",
+        example: true
+    })
+    @IsNotEmpty()
+    @IsBoolean()
+    active?: boolean
+}
