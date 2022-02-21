60ccfb3122ac7f2d256464106f5984741d844e54
jguibrandao <jguibrandao@gmail.com>
2022-02-21 17:43:31 -0300

    Making collaborators service (methods)

---
+++ src/collaborators/collaborators.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { CreateCollaboratorDto } from './dto/create-collaborator.dto';
+import { UpdateCollaboratorDto } from './dto/update-collaborator.dto';
+import { Collaborator } from './models/collaborators.model';
+import { Model } from 'mongoose';
+
+@Injectable()
+export class CollaboratorsService {
+  constructor(
+    @InjectModel('Collaborator')
+    private readonly collaboratorsModel: Model<Collaborator>,
+) 
+{}
+
+  public async createCollaborator(createCollaboratorDto: CreateCollaboratorDto): Promise<Collaborator> {
+    const collaborator = new this.collaboratorsModel(createCollaboratorDto)
+    return collaborator.save()
+  }
+
+  public async findAll() {
+    return this.collaboratorsModel.find()
+  }
+
+  public async findOne(id: any) {
+    return this.collaboratorsModel.findById(id)
+  }
+
+  public async update(id: string, updateCollaboratorDto: UpdateCollaboratorDto) {
+    return this.collaboratorsModel.findByIdAndUpdate(id, updateCollaboratorDto, { new: true })
+  }
+
+  remove(id: string) {
+    return this.collaboratorsModel.findByIdAndDelete(id)
+  }
+}
