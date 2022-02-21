5dd2fd9729fa5ec388fdf4219d83073afd0abfa4
jguibrandao <jguibrandao@gmail.com>
2022-02-21 17:42:34 -0300

    Importing modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,16 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ProjectsModule } from './projects/projects.module';
+import { CollaboratorsModule } from './collaborators/collaborators.module';
 
 @Module({
   imports: [
     UsersModule,
     AuthModule,
-    MongooseModule.forRoot(process.env.MONGO_URI)
+    MongooseModule.forRoot(process.env.MONGO_URI),
+    ProjectsModule,
+    CollaboratorsModule
   ],
   controllers: [AppController],
   providers: [AppService],
