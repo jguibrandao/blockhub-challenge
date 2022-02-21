03cd78bb46c7153491c0f32961b39077439cc2d8
jguibrandao <jguibrandao@gmail.com>
2022-02-21 18:23:42 -0300

    importing mongoosemodule

--- src/projects/projects.module.ts
+++ src/projects/projects.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ProjectsService } from './projects.service';
 import { ProjectsController } from './projects.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from 'src/users/users.module';
+import { ProjectsSchema } from './schemas/projects.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: "Project",
+        schema: ProjectsSchema
+      }
+    ]),
+
+    UsersModule
+  ],
   controllers: [ProjectsController],
   providers: [ProjectsService]
 })
